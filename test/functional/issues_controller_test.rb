eb14c7eb05cb9dd3075d24906a6802bfd670875e
Janek <jlachowicz@artegence.com>
2016-03-23 12:50:05 +0100

    add fixtures

---
+++ test/functional/issues_controller_test.rb
@@ -0,0 +1,27 @@
+require File.expand_path('../../test_helper', __FILE__)
+
+class HamstersControllerTest < Redmine::IntegrationTest
+  # self.fixture_path = File.join(File.dirname(__FILE__), '../fixtures')
+  # fixtures :users,
+  #          :issues,
+  #          :projects,
+  #          :issue_statuses,
+  #          :members,
+  #          :settings,
+  #          :roles,
+  #          :enabled_modules,
+  #          :time_entries,
+  #          :trackers,
+  #          :email_addresses,
+  #          :groups_users,
+  #          :member_roles,
+  #          :enumerations
+
+  # def setup
+  #
+  # end
+
+  def test_truth
+    assert true
+  end
+end
