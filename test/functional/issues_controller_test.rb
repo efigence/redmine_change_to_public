1503562d75acf7417d19313dadbc7c548f546925
Janek <jlachowicz@artegence.com>
2016-04-08 16:06:26 +0200

    add test

--- test/functional/issues_controller_test.rb
+++ test/functional/issues_controller_test.rb
@@ -1,27 +1,31 @@
 require File.expand_path('../../test_helper', __FILE__)
 
-class HamstersControllerTest < Redmine::IntegrationTest
-  # self.fixture_path = File.join(File.dirname(__FILE__), '../fixtures')
-  # fixtures :users,
-  #          :issues,
-  #          :projects,
-  #          :issue_statuses,
-  #          :members,
-  #          :settings,
-  #          :roles,
-  #          :enabled_modules,
-  #          :time_entries,
-  #          :trackers,
-  #          :email_addresses,
-  #          :groups_users,
-  #          :member_roles,
-  #          :enumerations
+class IssuesControllerTest < Redmine::IntegrationTest
+  self.fixture_path = File.join(File.dirname(__FILE__), '../fixtures')
+  fixtures :all
 
-  # def setup
-  #
-  # end
-
-  def test_truth
-    assert true
+  def setup
+    Journal.destroy_all
+    Journal.create(journalized_id: 1,
+                   journalized_type: "Issue",
+                   user_id: 2,
+                   notes: "Journal notes 1",
+                   created_on: Time.now - 3.days,
+                   private_notes: false)
+    Journal.create(journalized_id: 1,
+                   journalized_type: "Issue",
+                   user_id: 2,
+                   notes: "Journal notes 2",
+                   created_on: Time.now - 2.days,
+                   private_notes: true)
   end
+
+  def test_issue_show_action_shouldnt_assign_private_journals
+    log_user('admin', 'admin')
+    User.current.update(public_mode: true)
+    get issue_path(Issue.find(1))
+    assert_response 200
+    assert assigns(:journals).size, 1
+  end
+
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,2 @@
 # Load the Redmine helper
 require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
-
-ActiveSupport::TestCase.fixture_path=(File.expand_path("../fixtures",  __FILE__))
-ActiveSupport::TestCase.fixtures :all
